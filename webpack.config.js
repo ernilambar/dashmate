a0668b67f76a1b27a711ff696e5fae9d015a2141
Nilambar Sharma <nilambar@outlook.com>
2025-07-31 15:19:21 +0545

    Add hello app

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
+
+// Prevent cleaning output directory.
+const plugins = defaultConfig.plugins.filter(
+	( plugin ) => plugin.constructor.name !== 'CleanWebpackPlugin'
+);
+
+module.exports = {
+	...defaultConfig,
+	entry: {
+		index: './resources/index.js',
+	},
+	output: {
+		...defaultConfig.output,
+		filename: '[name].js',
+		chunkFilename: '[name].js',
+		path: require( 'path' ).resolve( __dirname, 'assets' ),
+		clean: false,
+	},
+	optimization: {
+		...defaultConfig.optimization,
+		splitChunks: false,
+	},
+	// Use filtered plugins
+	plugins: [ ...plugins ],
+	cache: {
+		type: 'filesystem',
+		buildDependencies: {
+			config: [ __filename ],
+		},
+	},
+};
