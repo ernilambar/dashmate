42a3bfb18c2e540597269c6e598f7390fd17c79f
Nilambar Sharma <nilambar@outlook.com>
2025-08-24 23:12:20 +0545

    Remove package

--- app/Admin/Admin_Page.php
+++ app/Admin/Admin_Page.php
@@ -7,10 +7,7 @@
 
 namespace Nilambar\Dashmate\Admin;
 
-use Nilambar\Dashmate\Core\Option;
-use Nilambar\Dashmate\Panels\SettingsPanel;
 use Nilambar\Dashmate\View\View;
-use Nilambar\Optify\Optify;
 
 /**
  * Admin_Page class.
@@ -26,7 +23,6 @@ class Admin_Page {
 	 */
 	public function __construct() {
 		add_action( 'admin_menu', [ $this, 'register_page' ] );
-		add_action( 'init', [ $this, 'register_settings' ] );
 		add_action( 'admin_enqueue_scripts', [ $this, 'enqueue_assets' ] );
 	}
 
@@ -54,20 +50,6 @@ class Admin_Page {
 	 */
 	public function render_page_content() {
 		View::render( 'pages/app' );
-	}
-
-	/**
-	 * Register settings
-	 *
-	 * @since 1.0.0
-	 */
-	public function register_settings() {
-		$optify = Optify::get_instance( 'dashmate', 'v1', DASHMATE_BASE_FILEPATH );
-		$optify->register_panel( 'dashmate-settings', SettingsPanel::class );
-		$optify->load_assets(
-			DASHMATE_DIR . '/vendor/ernilambar/optify/',
-			DASHMATE_URL . '/vendor/ernilambar/optify/'
-		);
 	}
 
 	/**
