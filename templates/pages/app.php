8f9367cbf310df4ba71f56869bede22a49baa219
Nilambar Sharma <nilambar@outlook.com>
2025-08-24 11:40:45 +0545

    Cleanup admin pages

--- templates/pages/app.php
+++ templates/pages/app.php
@@ -6,4 +6,8 @@
  */
 
 ?>
-<div id="dashmate-app"><?php esc_html_e( 'Loading...', 'dashmate' ); ?></div>
+
+<div class="wrap">
+	<h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
+	<div id="dashmate-app"><?php esc_html_e( 'Loading...', 'dashmate' ); ?></div>
+</div><!-- .wrap -->
