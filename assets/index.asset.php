2b9e4719f0defb46c974e09991b03ba46ec1d7e2
Nilambar Sharma <nilambar@outlook.com>
2025-10-16 21:23:11 +0545

    Update styles

--- assets/index.asset.php
+++ assets/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-i18n'), 'version' => '72c072d502e57bb090f0');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-i18n'), 'version' => '9c7afcf7015fb693872c');
